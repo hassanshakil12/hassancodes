8886b08e505477aa0c723eb98fc8164ab2e4fa3f
Muhammad Hassan <hassanshakil12@gmail.com>
2025-08-03 05:24:55 +0500

    Feat: Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,9 @@ const Navbar = () => {
   const navbarRef = useRef(null);
   const logoRef = useRef(null);
   const buttonRef = useRef(null);
+  const menuRef = useRef(null);
+  const menuBtnRef = useRef(null);
+  const sidebarRef = useRef(null);
 
   useEffect(() => {
     document.body.style.overflow = isOpen ? "hidden" : "auto";
@@ -59,6 +62,26 @@ const Navbar = () => {
         stagger: 0.2,
       }
     );
+
+    gsap.fromTo(
+      gsap.utils.toArray("#nav-link"),
+      { y: -100 },
+      {
+        y: 0,
+        duration: 1,
+        ease: "power1.inOut",
+        delay: 0.25,
+        stagger: 0.08,
+      }
+    );
+
+    gsap.set(gsap.utils.toArray("#res-nav-link"), {
+      x: -550,
+    });
+
+    gsap.set(menuBtnRef.current, { y: 100, opacity: 0 });
+
+    gsap.set(sidebarRef.current, { opacity: 0, x: -800 });
   }, []);
 
   return (
@@ -79,7 +102,9 @@ const Navbar = () => {
             <a
               key={item}
               href={`#${item.toLowerCase()}`}
+              id="nav-link"
               className="hover:text-green-500 transition-all duration-300"
+              ref={menuRef}
             >
               {item}
             </a>
@@ -90,54 +115,214 @@ const Navbar = () => {
           ref={buttonRef}
           className="w-[20%] flex justify-end items-center gap-4"
         >
-          <button className="hidden md:inline-block border-1 border-green-500 text-lg text-white px-8 py-2 rounded-full hover:text-neutral-900 hover:bg-green-500/90 transition-all duration-300 cursor-pointer">
+          <a
+            href="https://drive.google.com/file/d/1gLcAOEgiSXIkTLJDQJN8f6iOM_8_JfPM/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hidden md:inline-block border-1 border-green-500 text-lg text-white px-8 py-2 rounded-full hover:text-neutral-900 hover:bg-green-500/90 transition-all duration-300 cursor-pointer"
+          >
             Download Resume
-          </button>
+          </a>
 
           <button
-            className="md:hidden text-neutral-900 dark:text-white"
-            onClick={() => setIsOpen(true)}
+            className="md:hidden text-neutral-900 dark:text-white cursor-pointer hover:text-green-500 transition-all duration-300"
+            onClick={() => {
+              setIsOpen(true);
+              gsap.fromTo(
+                sidebarRef.current,
+                { opacity: 0, x: -800 },
+                {
+                  opacity: 1,
+                  x: 0,
+                  duration: 0.75,
+                  ease: "power3.inOut",
+                }
+              );
+              gsap.set(gsap.utils.toArray("#res-nav-link"), {
+                x: 0,
+                opacity: 1,
+                duration: 0.5,
+                ease: "power3.inOut",
+                stagger: 0.02,
+                delay: 0.25,
+              });
+              gsap.set(menuBtnRef.current, {
+                y: 0,
+                opacity: 1,
+                duration: 0.5,
+                delay: 0.75,
+                ease: "power2.inOut",
+              });
+            }}
             aria-label="Open menu"
           >
-            <Menu size={28} />
+            <Menu size={48} />
           </button>
         </div>
       </div>
 
       <div
-        className={`fixed inset-0 z-40 transition-opacity duration-300 bg-amber-50 ${
-          isOpen ? "visible opacity-100" : "invisible opacity-0"
-        }`}
+        ref={sidebarRef}
+        className={`fixed inset-0 z-999 transition-opacity duration-300 bg-green-500 h-screen md:hidden`}
       >
         <div
-          className={`absolute inset-0 bg-black/50 backdrop-blur-sm transition-opacity duration-500 ${
-            isOpen ? "opacity-100" : "opacity-0"
-          }`}
-          onClick={() => setIsOpen(false)}
+          className={`absolute inset-0 bg-black/50 backdrop-blur-sm transition-opacity duration-500`}
+          onClick={() => {
+            gsap.fromTo(
+              sidebarRef.current,
+              {
+                x: 0,
+                opacity: 1,
+                duration: 0.5,
+                ease: "power3.inOut",
+              },
+              { x: -800, opacity: 1 }
+            );
+            gsap.fromTo(
+              gsap.utils.toArray("#res-nav-link"),
+              {
+                x: 0,
+              },
+              {
+                x: -550,
+                duration: 0.1,
+                ease: "power3.inOut",
+                stagger: 0.02,
+              }
+            );
+            gsap.fromTo(
+              menuBtnRef.current,
+              {
+                y: 0,
+                opacity: 1,
+              },
+              {
+                y: 100,
+                opacity: 0,
+                duration: 0.1,
+                delay: 0.25,
+                ease: "power3.inOut",
+              }
+            );
+            setTimeout(() => {
+              setIsOpen(false);
+            }, 1000);
+          }}
         />
 
         <div
-          className={`absolute top-0 left-0 h-full w-full bg-amber-50 dark:bg-neutral-900 px-8 py-10 transition-transform duration-500 transform ${
-            isOpen ? "translate-x-0" : "-translate-x-full"
-          }`}
+          className={`absolute top-0 left-0 h-full w-full bg-amber-50 dark:bg-neutral-900 px-8 py-10 transition-transform duration-500 transform`}
         >
-          <div className="flex justify-end mb-10">
+          <div className="absolute top-11 right-11 z-99999">
             <button
-              onClick={() => setIsOpen(false)}
-              className="text-neutral-800 dark:text-white"
+              onClick={() => {
+                gsap.fromTo(
+                  sidebarRef.current,
+                  {
+                    x: 0,
+                    opacity: 1,
+                    duration: 0.5,
+                    ease: "power3.inOut",
+                  },
+                  {
+                    x: -800,
+                    opacity: 1,
+                    delay: 0.5,
+                    duration: 0.5,
+                    ease: "power3.inOut",
+                  }
+                );
+                gsap.fromTo(
+                  gsap.utils.toArray("#res-nav-link"),
+                  {
+                    x: 0,
+                  },
+                  {
+                    x: -550,
+                    duration: 0.1,
+                    ease: "power3.inOut",
+                    stagger: 0.02,
+                  }
+                );
+                gsap.fromTo(
+                  menuBtnRef.current,
+                  {
+                    y: 0,
+                    opacity: 1,
+                  },
+                  {
+                    y: 100,
+                    opacity: 0,
+                    duration: 0.1,
+                    delay: 0.25,
+                    ease: "power3.inOut",
+                  }
+                );
+                setTimeout(() => {
+                  setIsOpen(false);
+                }, 1000);
+              }}
+              className="text-neutral-800 dark:text-white cursor-pointer"
               aria-label="Close menu"
             >
-              <X size={28} />
+              <X size={48} />
             </button>
           </div>
 
-          <nav className="flex flex-col items-center justify-center space-y-6 text-xl font-medium text-neutral-800 dark:text-white">
+          <nav className="flex flex-col items-start justify-center space-y-6 font-medium text-neutral-800 dark:text-white">
             {navItems.map((item, index) => (
               <a
                 key={item}
                 href={`#${item.toLowerCase()}`}
-                onClick={() => setIsOpen(false)}
-                className="hover:text-green-500 transition-all duration-300"
+                onClick={() => {
+                  gsap.fromTo(
+                    sidebarRef.current,
+                    {
+                      x: 0,
+                      opacity: 1,
+                      duration: 0.5,
+                      ease: "power3.inOut",
+                    },
+                    {
+                      x: -800,
+                      opacity: 1,
+                      delay: 0.5,
+                      duration: 0.5,
+                      ease: "power3.inOut",
+                    }
+                  );
+                  gsap.fromTo(
+                    gsap.utils.toArray("#res-nav-link"),
+                    {
+                      x: 0,
+                    },
+                    {
+                      x: -550,
+                      duration: 0.1,
+                      ease: "power3.inOut",
+                      stagger: 0.02,
+                    }
+                  );
+                  gsap.fromTo(
+                    menuBtnRef.current,
+                    {
+                      y: 0,
+                      opacity: 1,
+                    },
+                    {
+                      y: 100,
+                      opacity: 0,
+                      duration: 0.1,
+                      delay: 0.25,
+                      ease: "power3.inOut",
+                    }
+                  );
+                  setTimeout(() => {
+                    setIsOpen(false);
+                  }, 1000);
+                }}
+                id="res-nav-link"
+                className="hover:text-green-500 transition-all duration-300 text-5xl"
                 style={{
                   transitionDelay: `${index * 60}ms`,
                 }}
@@ -146,9 +331,15 @@ const Navbar = () => {
               </a>
             ))}
 
-            <button className="mt-8 bg-green-600 text-white py-3 px-6 rounded-full hover:bg-green-700 transition">
+            <a
+              href="https://drive.google.com/file/d/1gLcAOEgiSXIkTLJDQJN8f6iOM_8_JfPM/view?usp=drive_link"
+              target="_blank"
+              rel="noopener noreferrer"
+              ref={menuBtnRef}
+              className="absolute bottom-10 text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl py-3 px-6 sm:py-4 sm:px-8 md:py-5 md:px-10 lg:py-6 lg:px-12 rounded-full border border-green-500 hover:bg-green-500 hover:text-neutral-900 transition-all duration-300 cursor-pointer"
+            >
               Download Resume
-            </button>
+            </a>
           </nav>
         </div>
       </div>
